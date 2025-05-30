0df3aa3b72f4e9a5203bf9b3d66e9fb97843efb1
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-05-30 09:34:54 +0300

    Refactored code to handle nested routes in home, app and userprofile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/users")
+      .then(resp => resp.json())
+      .then(usersArray => setUsers(usersArray))
+      .catch(error => console.log("SERVER ERROR", error))
+  }, [])
+
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      <Outlet context={users} />
     </>
   );
 };
